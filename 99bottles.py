0269b9f30e013f85d5f1587d877a4ab8f13600ab
superbat321 <pmthomas321@gmail.com>
2014-03-07 23:09:13 -0500

    Create 99bottles.py

---
+++ 99bottles.py
@@ -0,0 +1,3 @@
+for bottles in range(99, 1, -1):
+	n = bottles - 1
+	print "{0} bottles of beer on the wall, {0} bottles of beer...\n If one of those bottles happens to fall, {1} bottles of beer on the wall".format(bottles, n)
